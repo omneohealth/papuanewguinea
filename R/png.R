2fd4922a3581f0b7f27b6bfea8df19a3bbfaa17f
Ernest Guevarra <ernest@validmeasures.org>
2018-05-14 10:39:55 +0100

    update R scripts

---
+++ R/png.R
@@ -0,0 +1,126 @@
+################################################################################
+#
+#' png
+#'
+#' OMNeoHealth Datasets for Papua New Guinea
+#'
+#' This package contains various datasets collected for the Oxford Maternal and
+#' Neonatal Health project in Papua New Guinea.
+#'
+#' @docType package
+#' @name png
+#'
+#
+################################################################################
+NULL
+
+
+################################################################################
+#
+#' alotau_accord_2014
+#'
+#' Papua New Guinea's Alotau Accord for a Government of Reconciliation, Unity and Hope
+#'
+#' A dataset containing the text of Papua New Guinea's Alotau Accord for a
+#' Government of Reconciliation, Unity and Hope. The UTF-8 plain text is
+#' divided into elements of up to about 70 characters each. Some elements are
+#' blank.
+#'
+#' @format A character vector with 477 elements
+#'
+#' @source \url{https://srhr.org/abortion-policies/documents/countries/05-PNG-Policy-on-Free-Primary-Health-Care-an-Subsidized-Specialized-Health-Services-in-PNG-Department-of-Health-2013.pdf}
+#'
+#'
+#
+################################################################################
+"alotau_accord_2014"
+
+
+################################################################################
+#
+#' health_sector_gender_policy_2014
+#'
+#' Papua New Guinea's Health Sector Gender Policy 2014
+#'
+#' A dataset containing the text of Papua New Guinea's Health Sector Gender
+#' Policy 2014. The UTF-8 plain text is divided into elements of up to about 70
+#' characters each. Some elements are blank.
+#'
+#' @format A character vector with 698 elements
+#'
+#'
+#
+################################################################################
+"health_sector_gender_policy_2014"
+
+
+################################################################################
+#
+#' national_sexual_reproductive_policy
+#'
+#' Papua New Guinea's National Sexual Reproductive Policy
+#'
+#' A dataset containing the text of Papua New Guinea's National Sexual Reproductive
+#' Policy. The UTF-8 plain text is divided into elements of up to about 70
+#' characters each. Some elements are blank.
+#'
+#' @format A character vector with 712 elements
+#'
+#'
+#
+################################################################################
+"national_sexual_reproductive_policy"
+
+
+################################################################################
+#
+#' newborn_health_policy_2014
+#'
+#' Papua New Guinea's Newborn Health Policy 2014
+#'
+#' A dataset containing the text of Papua New Guinea's Newborn Health Policy
+#' 2014. The UTF-8 plain text is divided into elements of up to about 70
+#' characters each. Some elements are blank.
+#'
+#' @format A character vector with 1331 elements
+#'
+#'
+#
+################################################################################
+"newborn_health_policy_2014"
+
+
+################################################################################
+#
+#' png_vision_2015
+#'
+#' Papua New Guinea Vision 2050
+#'
+#' A dataset containing the text of Papua New Guinea Vision 2050. The UTF-8
+#' plain text is divided into elements of up to about 70 characters each. Some
+#' elements are blank.
+#'
+#' @format A character vector with 2819 elements
+#'
+#'
+#
+################################################################################
+"png_vision_2015"
+
+
+################################################################################
+#
+#' taskforce_maternal_health_2009
+#'
+#' Ministerial Taskforce on Maternal Health in Papua New Guinea
+#'
+#' A dataset containing the text of Ministerial Taskforce on Maternal Health in
+#' Papua New Guinea. The UTF-8 plain text is divided into elements of up to about
+#' 70 characters each. Some elements are blank.
+#'
+#' @format A character vector with 3178 elements
+#'
+#'
+#
+################################################################################
+"taskforce_maternal_health_2009"
