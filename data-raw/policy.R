d54a7887d87726f2a7a12ffeab405e87011d6b36
Ernest Guevarra <ernest@validmeasures.org>
2018-05-21 07:08:07 +0100

    update policy.R

--- data-raw/policy.R
+++ data-raw/policy.R
@@ -86,7 +86,7 @@ for(i in 1:length(x)) {
   taskforce_maternal_health_2009 <- c(taskforce_maternal_health_2009, temp)
 }
 
-taskforce_maternal_health_2009 <- data_frame(line = 1:length(taskforce_maternal_health_2009),
+taskforce_maternal_health_2009 <- data_frame(linenumber = 1:length(taskforce_maternal_health_2009),
                                              text = taskforce_maternal_health_2009)
 
 devtools::use_data(taskforce_maternal_health_2009, overwrite = TRUE)
@@ -108,7 +108,7 @@ for(i in 1:length(x)) {
   national_sexual_reproductive_policy <- c(national_sexual_reproductive_policy, temp)
 }
 
-national_sexual_reproductive_policy <- data_frame(line = 1:length(national_sexual_reproductive_policy),
+national_sexual_reproductive_policy <- data_frame(linenumber = 1:length(national_sexual_reproductive_policy),
                                                   text = national_sexual_reproductive_policy)
 
 devtools::use_data(national_sexual_reproductive_policy, overwrite = TRUE)
@@ -130,7 +130,7 @@ for(i in 1:length(x)) {
   youth_adolescents_health_policy_2014 <- c(youth_adolescents_health_policy_2014, temp)
 }
 
-youth_adolescents_health_policy_2014 <- data_frame(line = 1:length(youth_adolescents_health_policy_2014),
+youth_adolescents_health_policy_2014 <- data_frame(linenumber = 1:length(youth_adolescents_health_policy_2014),
                                                    text = youth_adolescents_health_policy_2014)
 
 devtools::use_data(youth_adolescents_health_policy_2014, overwrite = TRUE)
@@ -152,7 +152,7 @@ for(i in 1:length(x)) {
   newborn_health_policy_2014 <- c(newborn_health_policy_2014, temp)
 }
 
-newborn_health_policy_2014 <- data_frame(line = 1:length(newborn_health_policy_2014),
+newborn_health_policy_2014 <- data_frame(linenumber = 1:length(newborn_health_policy_2014),
                                          text = newborn_health_policy_2014)
 
 devtools::use_data(newborn_health_policy_2014, overwrite = TRUE)
